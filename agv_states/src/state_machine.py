9de1d54d563f00befd30706f590fc43061da8fbb
Tevin <deskcomp10@outlook.com>
2022-10-18 20:48:19 -0400

    Modified message package

--- agv_states/src/state_machine.py
+++ agv_states/src/state_machine.py
@@ -4,14 +4,14 @@
 import rospy
 import actionlib
 import smach, smach_ros
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionGoal
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionGoal
 
 # ROS messages
 import std_msgs.msg as std
 import nav_msgs.msg as nav
 import sensor_msgs.msg as sens
 import geometry_msgs.msg as geom
-import move_base_msgs.msg as move_base
+# import move_base_msgs.msg as move_base
 import ugv_msg.msg as ugv
 
 # import states
